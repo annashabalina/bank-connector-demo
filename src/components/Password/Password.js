c196d8e5390f46c432a8eec2b30759060c16c2ca
Anna Shabalina <anna.shabalina@finreach.com>
2019-10-08 16:36:46 +0200

    Add styles for Password component
    Change font color of 'Show' and the eye icon
    Use Dosis as font-family
    Change input's border color when input is focused

--- src/components/Password/Password.js
+++ src/components/Password/Password.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import {Icon} from 'antd';
 import {useToggle} from "hooks/useToggle";
+import passwordStyle from './Password.module.scss';
 
 export const Password = () => {
     const {isOn: showPlainText, toggle} = useToggle();
 
     return (
-        <>
-            <Icon
-                type={showPlainText ? 'eye-invisible' : 'eye'}
-                onClick={toggle}
-            />
+        <div className={passwordStyle.passwordWrapper}>
+            <div>
+                <span>
+                <Icon
+                    type={showPlainText ? 'eye-invisible' : 'eye'}
+                    onClick={toggle}
+                />
+                Show
+                </span>
+            </div>
             <label> Password
                 <input type={showPlainText ? 'text' : 'password'}
                        name='password'/>
             </label>
-        </>
+        </div>
     )
 };
 
